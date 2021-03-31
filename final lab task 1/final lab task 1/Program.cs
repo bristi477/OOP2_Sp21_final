e273dcfeea3b07ec4298ea614d71eea6e88a70bf
Bristyy <bristigayali1998@gmail.com>
2021-03-31 20:27:44 +0600

    final

---
+++ final lab task 1/final lab task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace final_lab_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ final lab task 1/final lab task 1/class course.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace final_lab_task_1
+{
+    class class_course
+    {
+    }
+}
